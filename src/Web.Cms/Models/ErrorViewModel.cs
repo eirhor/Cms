9836508f0a23166342be27f1d769b687be56798b
Eirik <horvath.eirik@gmail.com>
2018-11-18 13:16:48 +0100

    Create Web projects and initialization pattern.

---
+++ src/Web.Cms/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web.Cms.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/Web/InitializationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Web
+{
+    public static class InitializationExtensions
+    {
+        public static IServiceCollection AddCmsWeb(this IServiceCollection services)
+        {
+            var optionsSetup = new ConfigureOptions<RazorViewEngineOptions>(options =>
+            {
+                // TODO: Add providers for CMS as described here: https://github.com/aspnet/AspNetCore/issues/1490#issuecomment-221676623
+            });
+
+            services.AddSingleton<IConfigureOptions<RazorViewEngineOptions>>(optionsSetup);
+
+            return services;
+        }
+    }
+}
